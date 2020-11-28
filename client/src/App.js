27d69dd8a2c9eed753ee7920bc6eb3934dc7c3c2
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-11-28 17:33:12 +0100

    Create basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
-}
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" component={Chat} />
+  </Router>
+);
 
 export default App;
