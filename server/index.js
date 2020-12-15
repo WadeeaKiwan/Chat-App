6127797ca1dfb8573e30caa071e8de34c4ab5e1d
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-12-15 12:08:21 +0100

    Disconnect user when leaving the chat

--- server/index.js
+++ server/index.js
@@ -41,6 +41,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left!` });
+    }
+
     console.log('User had left!!!');
   })
 });
