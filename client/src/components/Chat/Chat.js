89fc59822c2609ead0eceecc7150d57cea67220f
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-12-18 17:03:35 +0100

    Remove /api route from server url

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   // const ENDPOINT = 'http://localhost:5000';
-  const ENDPOINT = 'https://chat-app-2021.herokuapp.com/api';
+  const ENDPOINT = 'https://chat-app-2021.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
