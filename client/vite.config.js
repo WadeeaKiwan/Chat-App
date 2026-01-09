e0aae6f8ecad60cb5bda5629d4ed4ce9b10a1202
Wadeea Kiwan <wadeea.kiwan@hotmail.com>
2026-01-10 00:44:18 +0100

    Update build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',      // change from default "dist"
+    emptyOutDir: true     // clears build folder before build
+  },
   plugins: [react()],
   server: {
     port: 3000
